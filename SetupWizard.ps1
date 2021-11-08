1aa5167ce6744aa88d2ff14cefa55aaa9c119b8a
MediocreSoftware <74843158+Mediocre-Software@users.noreply.github.com>
2021-11-08 10:48:57 -0600

    Add files via upload

--- SetupWizard.ps1
+++ SetupWizard.ps1
@@ -1,6 +1,7 @@
 #====================================================#
 #############    Hide Console Window     #############
 #====================================================#
+Import-Module ScheduledTasks
 Import-Module PowerShellGet
 Import-Module SecureBoot
 Add-Type -Name Window -Namespace Console -MemberDefinition '
@@ -13,7 +14,6 @@ $consolePtr = [Console.Window]::GetConsoleWindow()
 
 
 Function Show-SetupWizard {
-	
 	
 	#====================================================#
 	#############    Imported Assemblies    ##############
@@ -37,7 +37,6 @@ Function Show-SetupWizard {
 	$cbPsVersion = New-Object 'System.Windows.Forms.CheckBox'
 	$cbGoogle = New-Object 'System.Windows.Forms.CheckBox'
 	$cbGitHub = New-Object 'System.Windows.Forms.CheckBox'
-	$picturebox1 = New-Object 'System.Windows.Forms.PictureBox'
 	$progressbar1 = New-Object 'System.Windows.Forms.ProgressBar'
 	$cbExcel = New-Object 'System.Windows.Forms.CheckBox'
 	$cbDatabase = New-Object 'System.Windows.Forms.CheckBox'
@@ -52,74 +51,101 @@ Function Show-SetupWizard {
 	$form2_Load = {
 		$okCancel = [System.Windows.Forms.MessageBox]::Show("This wizard will install all the necassary modules and updates required for the parts database. `Click OK to continue or Cancel to exit the setup", "System Update", "OKCancel", "Information")
 		If ($okCancel -eq [System.Windows.Forms.DialogResult]::OK) {
-			$okUpdate = [System.Windows.Forms.MessageBox]::Show("Follow the prompts that may appear after successful downloads to complete the installation of those components.", "System Update", "OK", "Information")
-			If ($okUpdate -eq [System.Windows.Forms.DialogResult]::OK) {
-				$progressbar1.ForeColor = 'CornflowerBlue'
-				$progressbar1.Enabled = $true
-				Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force | Out-Null
-				$cbPolicy.Checked = $True
-				$textbox1.Text = "Local machine's PS Execution Policy Change -- Complete!"
-				$g = (Get-Host).Version
-				If ($g.Major -le 5) {
-					Invoke-Exp "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI" | Out-Null
-				}
-				$cbPsVersion.Checked = $true
-				$textbox1.Text = "Install Powershell 7.1.5 -- Complete!"
-				$githubCheck = (Get-Module -ListAvailable -Name 'PowerShellForGitHub')
-				If ($null -eq $githubCheck) {
-					Install-Module 'PowerShellForGitHub' -Scope CurrentUser -AllowClobber -Force | Out-Null
-				}
-				$cbGitHub.Checked = $true
-				$textbox1.Text = "Install GitHub Rest API Module -- Complete!"
-				$googleCheck = (Get-Module -ListAvailable -Name 'UMN-Google')
-				If ($null -eq $googleCheck) {
-					Install-Module 'UMN-Google' -Scope CurrentUser -AllowClobber -Force | Out-Null
-				}
-				$cbGoogle.Checked = $true
-				$textbox1.Text = "Install Google Rest API Module -- Complete!"
-				$excelCheck = (Get-Module -ListAvailable -Name 'ImportExcel')
-				If ($null -eq $excelCheck) {
-					Install-Module 'ImportExcel' -Scope CurrentUser -AllowClobber -Force | Out-Null
-				}
-				$cbExcel.Checked = $true
-				$textbox1.Text = "Install Microsoft Excel .NET Module -- Complete!"
-				$dpcPath = Join-Path "$($env:APPDATA)" "DPC_PartsDB"
-				$dpcPathExists = Test-Path $dpcPath
-				If ($dpcPathExists -eq $false) {
-					New-Item -Path "$($env:APPDATA)" -Name "DPC_PartsDB" -ItemType "Directory" | Out-Null
-				}
-				$changeDir = Join-Path "$($env:APPDATA)" "DPC_PartsDB/Change_Log"
-				$changeExists = Test-Path $changeDir
-				If ($changeExists -eq $false) {
-					New-Item -Path "$($env:APPDATA)/DPC_PartsDB" -Name "Change_Log" -ItemType "Directory" | Out-Null
-				}
-				$textbox1.Text = "Create Database directories in local File System -- Complete!"
-				$icoPath = "$($env:APPDATA)/DPC_PartsDB/DataTable.ico"
-				$icoExists = Test-Path $icoPath
-				If ($icoExists -ne $True) {
-					(Invoke-WebRequest -Uri https://raw.githubusercontent.com/Mediocre-Software/DPC_PartsDB/main/DataTable.ico -OutFile "$($env:APPDATA)/DPC_PartsDB/DataTable.ico")
-				}
-				$logoPath = "$($env:APPDATA)/DPC_PartsDB/DPCLogo.png"
-				$logoExists = Test-Path $logoPath
-				If ($logoExists -ne $True) {
-					(Invoke-WebRequest -Uri https://raw.githubusercontent.com/Mediocre-Software/DPC_PartsDB/main/DPCLogo.png -OutFile "$($env:APPDATA)/DPC_PartsDB/DPCLogo.png")
-				}
-				$firstRun = "$($env:APPDATA)/DPC_PartsDB/FirstRun.Check"
-					(Invoke-WebRequest -Uri https://raw.githubusercontent.com/Mediocre-Software/DPC_PartsDB/main/FirstRun.Check -OutFile "$($env:APPDATA)/DPC_PartsDB/FirstRun.Check")
-				$cbDatabase.Checked = $true
-				$textbox1.Text = "Download and install database dependencies -- Complete!"
-				Start-Sleep -Seconds '2' 
-				$progressbar1.Enabled = $false
-				$okExit = [System.Windows.Forms.MessageBox]::Show("All updates have been successfully completed. Press OK to exit this wizard and launch the Parts Database.", "Updates Complete", 'OK')
-				If ($okExit -eq [System.Windows.Forms.DialogResult]::OK) {
-					$form2.Close
-				}
+			$form2.Visible = $true
+			$form2.TopLevel = $true
+			$form2.TopMost = $true
+			$textbox1.Text = "Setting Execution Policy for PowerShell..."
+			$progressbar1.pers
+			Start-Sleep -Seconds 2
+			(Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force)
+			$cbPolicy.Checked = $True
+			$textbox1.Text = "Local machine's PS Execution Policy Change -- Complete!"
+			Start-Sleep -Seconds '3'
+			$g = (Get-Host).Version
+			If ($g.Major -le 5) {
+				$textbox1.Text = "Updating PowerShell to version 7.1.5"
+				(Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI")
 			}
+			$cbPsVersion.Checked = $true
+			$textbox1.Text = "Install Powershell 7.1.5 -- Complete!"
+			Start-Sleep -Seconds '3'
+			$githubCheck = (Get-Module -ListAvailable -Name 'PowerShellForGitHub')
+			If ($null -eq $githubCheck) {
+				$textbox1.Text = "Installing GitHub API Module..."
+				(Install-Module 'PowerShellForGitHub' -Scope CurrentUser -AllowClobber -Force)
+			}
+			$cbGitHub.Checked = $true
+			$textbox1.Text = "Install GitHub Rest API Module -- Complete!"
+			Start-Sleep -Seconds '3'
+			$googleCheck = (Get-Module -ListAvailable -Name 'UMN-Google')
+			If ($null -eq $googleCheck) {
+				$textbox1.Text = "Installing Google API Module..."
+				(Install-Module 'UMN-Google' -Scope CurrentUser -AllowClobber -Force)
+			}
+			$cbGoogle.Checked = $true
+			$textbox1.Text = "Install Google Rest API Module -- Complete!"
+			Start-Sleep -Seconds '3'
+			$excelCheck = (Get-Module -ListAvailable -Name 'ImportExcel')
+			If ($null -eq $excelCheck) {
+				$textbox1.Text = "Installing MS Excel Module..."
+				(Install-Module 'ImportExcel' -Scope CurrentUser -AllowClobber -Force)
+			}
+			$cbExcel.Checked = $true
+			$textbox1.Text = "Install Microsoft Excel .NET Module -- Complete!"
+			Start-Sleep -Seconds '3'
+			$dpcPath = Join-Path "$($env:APPDATA)" "DPC_PartsDB"
+			$dpcPathExists = Test-Path $dpcPath
+			$textbox1.Text = "Installing Database Components..."
+			If ($dpcPathExists -eq $false) {
+				(New-Item -Path "$($env:APPDATA)" -Name "DPC_PartsDB" -ItemType "Directory")
+			}
+			$changeDir = Join-Path "$($env:APPDATA)" "DPC_PartsDB/Change_Log"
+			$changeExists = Test-Path $changeDir
+			If ($changeExists -eq $false) {
+				(New-Item -Path "$($env:APPDATA)/DPC_PartsDB" -Name "Change_Log" -ItemType "Directory")
+			}
+			$textbox1.Text = "Create Database directories in local File System -- Complete!"
+			Start-Sleep -Seconds '3'
+			$icoPath = "$($env:APPDATA)/DPC_PartsDB/DataTable.ico"
+			$icoExists = Test-Path $icoPath
+			If ($icoExists -ne $True) {
+				(Invoke-WebRequest -Uri https://raw.githubusercontent.com/Mediocre-Software/DPC_PartsDB/main/DataTable.ico -OutFile "$($env:APPDATA)/DPC_PartsDB/DataTable.ico")
+			}
+			$logoPath = "$($env:APPDATA)/DPC_PartsDB/DPCLogo.png"
+			$logoExists = Test-Path $logoPath
+			If ($logoExists -ne $True) {
+				(Invoke-WebRequest -Uri https://raw.githubusercontent.com/Mediocre-Software/DPC_PartsDB/main/DPCLogo.png -OutFile "$($env:APPDATA)/DPC_PartsDB/DPCLogo.png")
+			}
+			$firstRun = "$($env:APPDATA)/DPC_PartsDB/FirstRun.Check"
+			$firstRunExists = Test-Path $firstRun
+			If ($firstRunExists -ne $true) {
+				(Invoke-WebRequest -Uri https://raw.githubusercontent.com/Mediocre-Software/DPC_PartsDB/main/FirstRun.Check -OutFile "$($env:APPDATA)/DPC_PartsDB/FirstRun.Check")
+			}
+			$licPath = "$($env:APPDATA)/DPC_PartsDB/GPL3.0.txt"
+			$licExists = Test-Path $licPath
+			If ($licExists -ne $true) {
+				(Invoke-WebRequest -Uri https://raw.githubusercontent.com/Mediocre-Software/DPC_PartsDB/main/GPL3.0.txt -OutFile "$($env:APPDATA)/DPC_PartsDB/GPL3.0.txt")
+			}
+			$aboutPath = "$($env:APPDATA)/DPC_PartsDB/About.ps1"
+			$aboutExists = Test-Path $aboutPath
+			If ($aboutExists -ne $true) {
+				(Invoke-WebRequest -Uri https://raw.githubusercontent.com/Mediocre-Software/DPC_PartsDB/main/About.ps1 -OutFile "$($env:APPDATA)/DPC_PartsDB/About.ps1")
+			}
+			$cbDatabase.Checked = $true
+			$textbox1.Text = "Download and install database dependencies -- Complete!"
+			Start-Sleep -Seconds '3'
 		} Else {
 			$form2.Close
 		}
+		
+		$okExit = [System.Windows.Forms.MessageBox]::Show("All updates have been successfully completed. Press OK to exit this wizard and launch the Parts Database.", "Updates Complete", 'OK')
+		If ($okExit -eq [System.Windows.Forms.DialogResult]::OK) {
+			$scriptPath = $MyInvocation.PSScriptRoot
+			$partsDB = (Join-Path $scriptPath '/DPC_PartsDB.ps1')
+			(Invoke-Item -Path $partsDB) | Out-Null
+			$form2.Close()
+		}
 	}
-	
 	
 	$Form_StateCorrection_Load =
 	{
@@ -131,8 +157,7 @@ Function Show-SetupWizard {
 	{
 		#Remove all event handlers from the controls
 		Try {
-			$textbox1.remove_TextChanged($textbox1_TextChanged)
-			$form2.remove_Load($formSplashScreen_Load)
+			$form2.remove_Load($form2_Load)
 			$form2.remove_Load($Form_StateCorrection_Load)
 			$form2.remove_FormClosed($Form_Cleanup_FormClosed)
 		} Catch {
@@ -151,21 +176,28 @@ Function Show-SetupWizard {
 	#
 	# formDPCSetupWizard
 	#
+	$form2.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon("$($env:APPDATA)/DPC_PartsDB/DataTable.ico")
 	$form2.Controls.Add($panel2)
 	$form2.Controls.Add($panel1)
 	$form2.AutoScaleDimensions = '9, 19'
 	$form2.AutoScaleMode = 'Font'
 	$form2.BackColor = 'ControlLight'
-	$form2.ClientSize = '423, 228'
+	$form2.ClientSize = '423, 200'
+	$form2.Cursor = 'Default'
 	$form2.Font = 'Consolas, 9pt'
 	$form2.FormBorderStyle = 'FixedSingle'
 	$form2.Margin = '3, 3, 3, 3'
 	$form2.MaximizeBox = $False
-	$form2.Name = 'formDPCSetupWizard'
+	$form2.MinimizeBox = $true
+	$form2.WindowState = 'Normal'
+	$form2.TopMost = $true
+	$form2.Name = 'form2'
+	$form2.ShowIcon = $True
+	$form2.ShowInTaskbar = $True
 	$form2.SizeGripStyle = 'Hide'
 	$form2.StartPosition = 'CenterScreen'
 	$form2.Text = 'DiscountPC DB-Setup Wizard'
-	$form2.TopMost = $True
+	$form2.Enabled = $true
 	$form2.add_Load($form2_Load)
 	#
 	# panel2
@@ -176,7 +208,7 @@ Function Show-SetupWizard {
 	$panel2.Margin = '4, 4, 4, 4'
 	$panel2.Name = 'panel2'
 	$panel2.Size = '403, 52'
-	$panel2.TabIndex = 7
+	$panel2.TabIndex = 3
 	#
 	# textbox1
 	#
@@ -191,9 +223,8 @@ Function Show-SetupWizard {
 	$textbox1.ReadOnly = $True
 	$textbox1.ShortcutsEnabled = $False
 	$textbox1.Size = '377, 28'
-	$textbox1.TabIndex = 6
-	$textbox1.Text = 'System check completed -- Updates Required `
-Awaiting user-input.'
+	$textbox1.TabIndex = 4
+	$textbox1.Text = "System check completed -- Updates Required. Awaiting user-input."
 	$textbox1.TextAlign = 'Center'
 	#
 	# panel1
@@ -202,7 +233,6 @@ Awaiting user-input.'
 	$panel1.Controls.Add($cbPsVersion)
 	$panel1.Controls.Add($cbGoogle)
 	$panel1.Controls.Add($cbGitHub)
-	$panel1.Controls.Add($picturebox1)
 	$panel1.Controls.Add($progressbar1)
 	$panel1.Controls.Add($cbExcel)
 	$panel1.Controls.Add($cbDatabase)
@@ -211,12 +241,13 @@ Awaiting user-input.'
 	$panel1.Margin = '4, 4, 4, 4'
 	$panel1.Name = 'panel1'
 	$panel1.Size = '405, 154'
-	$panel1.TabIndex = 6
+	$panel1.TabIndex = 12
 	#
 	# checkboxPowerShellSetExecuti
 	#
 	$cbPolicy.AutoCheck = $false
 	$cbPolicy.AutoSize = $True
+	$cbPolicy.Checked = $false
 	$cbPolicy.FlatAppearance.CheckedBackColor = 'DodgerBlue'
 	$cbPolicy.FlatStyle = 'System'
 	$cbPolicy.Font = 'Lucida Console, 7pt'
@@ -224,16 +255,16 @@ Awaiting user-input.'
 	$cbPolicy.Margin = '4, 4, 4, 4'
 	$cbPolicy.Name = 'cbPolicy'
 	$cbPolicy.Size = '184,16'
-	$cbPolicy.TabIndex = 28
+	$cbPolicy.TabIndex = 5
 	$cbPolicy.Text = 'Local Execution Policy'
 	$cbPolicy.UseCompatibleTextRendering = $True
 	$cbPolicy.UseVisualStyleBackColor = $True
 	#
 	# checkboxPowerShellVersion7Or
 	#
-	$cbPsVersion.AutoCheck = $false
 	$cbPsVersion.AutoCheck = $False
 	$cbPsVersion.AutoSize = $True
+	$cbPsVersion.Checked = $False
 	$cbPsVersion.FlatAppearance.CheckedBackColor = 'DodgerBlue'
 	$cbPsVersion.FlatStyle = 'System'
 	$cbPsVersion.Font = 'Lucida Console, 7pt'
@@ -242,7 +273,7 @@ Awaiting user-input.'
 	$cbPsVersion.Margin = '4, 4, 4, 4'
 	$cbPsVersion.Name = 'cbPsVersion'
 	$cbPsVersion.Size = '184, 16'
-	$cbPsVersion.TabIndex = 27
+	$cbPsVersion.TabIndex = 6
 	$cbPsVersion.Text = 'PowerShell 7.1.5'
 	$cbPsVersion.UseCompatibleTextRendering = $True
 	$cbPsVersion.UseVisualStyleBackColor = $True
@@ -251,6 +282,7 @@ Awaiting user-input.'
 	#
 	$cbGoogle.AutoCheck = $false
 	$cbGoogle.AutoSize = $True
+	$cbGoogle.Checked = $False
 	$cbGoogle.FlatAppearance.CheckedBackColor = 'DodgerBlue'
 	$cbGoogle.FlatStyle = 'System'
 	$cbGoogle.Font = 'Lucida Console, 7pt'
@@ -258,7 +290,7 @@ Awaiting user-input.'
 	$cbGoogle.Margin = '4, 4, 4, 4'
 	$cbGoogle.Name = 'cbGoogle'
 	$cbGoogle.Size = '184, 16'
-	$cbGoogle.TabIndex = 25
+	$cbGoogle.TabIndex = 7
 	$cbGoogle.Text = 'Google API Module'
 	$cbGoogle.UseCompatibleTextRendering = $True
 	$cbGoogle.UseVisualStyleBackColor = $True
@@ -267,6 +299,7 @@ Awaiting user-input.'
 	#
 	$cbGitHub.AutoCheck = $false
 	$cbGitHub.AutoSize = $True
+	$cbGitHub.Checked = $False
 	$cbGitHub.FlatAppearance.CheckedBackColor = 'DodgerBlue'
 	$cbGitHub.FlatStyle = 'System'
 	$cbGitHub.Font = 'Lucida Console, 7pt'
@@ -274,7 +307,7 @@ Awaiting user-input.'
 	$cbGitHub.Margin = '4, 4, 4, 4'
 	$cbGitHub.Name = 'cbGitHub'
 	$cbGitHub.Size = '184, 16'
-	$cbGitHub.TabIndex = 24
+	$cbGitHub.TabIndex = 8
 	$cbGitHub.Text = 'GitHub API Module'
 	$cbGitHub.UseCompatibleTextRendering = $True
 	$cbGitHub.UseVisualStyleBackColor = $True
@@ -283,6 +316,7 @@ Awaiting user-input.'
 	#
 	$cbExcel.AutoCheck = $false
 	$cbExcel.AutoSize = $True
+	$cbExcel.Checked = $False
 	$cbExcel.FlatAppearance.CheckedBackColor = 'DodgerBlue'
 	$cbExcel.FlatStyle = 'System'
 	$cbExcel.Font = 'Lucida Console, 7pt'
@@ -290,7 +324,7 @@ Awaiting user-input.'
 	$cbExcel.Margin = '4, 4, 4, 4'
 	$cbExcel.Name = 'cbExcel'
 	$cbExcel.Size = '184, 16'
-	$cbExcel.TabIndex = 26
+	$cbExcel.TabIndex = 9
 	$cbExcel.Text = 'MS Excel Module'
 	$cbExcel.UseCompatibleTextRendering = $True
 	$cbExcel.UseVisualStyleBackColor = $True
@@ -299,6 +333,7 @@ Awaiting user-input.'
 	# 
 	$cbDatabase.AutoCheck = $false
 	$cbDatabase.AutoSize = $True
+	$cbDatabase.Checked = $False
 	$cbDatabase.FlatAppearance.CheckedBackColor = 'DodgerBlue'
 	$cbDatabase.FlatStyle = 'System'
 	$cbDatabase.Font = 'Lucida Console, 7pt'
@@ -306,7 +341,7 @@ Awaiting user-input.'
 	$cbDatabase.Margin = '4, 4, 4, 4'
 	$cbDatabase.Name = 'cbDatabase'
 	$cbDatabase.Size = '184, 16'
-	$cbDatabase.TabIndex = 26
+	$cbDatabase.TabIndex = 10
 	$cbDatabase.Text = 'DPC Parts Database'
 	$cbDatabase.UseCompatibleTextRendering = $True
 	$cbDatabase.UseVisualStyleBackColor = $True
@@ -314,17 +349,14 @@ Awaiting user-input.'
 	# progressbar1
 	#
 	$progressbar1.Dock = 'Bottom'
-	$progressbar1.Location = '0, 130'
 	$progressbar1.Margin = '4, 4, 4, 4'
 	$progressbar1.Name = 'progressbar1'
 	$progressbar1.Size = '401, 20'
-	$progressbar1.TabIndex = 29
-	$progressbar1.ForeColor = 'CornflowerBlue'
+	$progressbar1.TabIndex = 0
 	$progressbar1.Style = 'Marquee'
-	$progressbar1.MarqueeAnimationSpeed = '85'
-	$progressbar1.Minimum = '0'
-	$progressbar1.Maximum = '100'
-	$progressbar1.Enabled = $false
+	$progressbar1.MarqueeAnimationSpeed = '185'
+	$progressbar1.Enabled = $true
+	$progressbar1.Visible = $true
 	
 	$panel2.ResumeLayout()
 	$panel1.ResumeLayout()
